0e5003232fd29b2ea35e6d2b1435fbaf13cda29b
Zach Burke <zachburke@knights.ucf.edu>
2020-09-30 20:36:41 -0400

    Updated

--- Burke_p2/src/bmi/App.java
+++ Burke_p2/src/bmi/App.java
@@ -3,6 +3,7 @@ package bmi;
 import bmi.BodyMassIndex;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
@@ -23,34 +24,76 @@ public class App {
 
 	    displayBmiStatistics(bmiData);
 	}
-	private static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData) {
-		// TODO Auto-generated method stub
-		
-	}
-	private static boolean moreInput() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	public static void displayBmiInfo(BodyMassIndex bmi) {
-		
-		
-	}
-
+	
 	public static double getUserWeight() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter your Weight: ");
-		scanner.nextDouble();
-		return getUserWeight();
+		
+		double weight = 0;
+		while(weight > 0) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Please enter your weight in pounds: ");
+			weight = scan.nextDouble();
+			
+		}
+		return weight;
+	
 	}
+	
 	public static double getUserHeight() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter your Height: ");
-		scanner.nextDouble();
-		return getUserHeight();
+		
+		double height = 0;
+		while(height > 0) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Please enter your height in inches: ");
+			height = scan.nextDouble();
+			scan.nextLine();
+		}
+		return height;
+	
+	}
+	
+	public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData) {
+		
+		bmiData.addAll(10, bmiData);
+		bmiData.addAll(15, bmiData);
+		bmiData.addAll(20, bmiData);
+		int total = 0;
+		int avg;
+		for(int i = 0; i < bmiData.size(); i++)
+		{
+		    total = bmiData.get(i);
+		    avg = total / bmiData.size();
+		    System.out.println("The Average BMI score:" + avg);
+		}
+	
+		
+	}
+
+	private static void displayBmiInfo(BodyMassIndex bmi) {
+		
+		System.out.println("Your BMI is: " + BodyMassIndex.bmi);
+		System.out.println(BodyMassIndex.Category);
+		
 	}
 
 
 
+	public static boolean moreInput() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Would you like to continue? (Enter 'y' for yes, and 'n' for no) ");
+		String choice = scan.nextLine();
+		if(choice.equals("y") ) {
+			return true;
+		} else {
+		return false;
+		}
+		
+	}
+
+
+
+
+	
+	
 
 
 
--- Burke_p2/src/bmi/BodyMassIndex.java
+++ Burke_p2/src/bmi/BodyMassIndex.java
@@ -6,19 +6,35 @@ public class BodyMassIndex {
 	
 	private double Height;
 	private double Weight;
-	private double bmi;
-	
+	static double bmi;
+	static String Category;
 	
 	public BodyMassIndex(double Height, double Weight) {
 		
 		this.Height = Height;
 		this.Weight = Weight;
-		bmi = (Weight * 703) / (Height * Height);
+		
 	}
 
 
 	public double getBmi() {
+		bmi = (Weight * 703) / (Height * Height);
 		return bmi;
+	}
+
+
+	public String getCategory() {
+		if(bmi < 18.5) {
+			System.out.println("You are underweight.");
+		} else if(bmi < 24.91) {
+			System.out.println("You are normal weight.");
+		} else if(bmi < 29.91) {
+			System.out.println("You are overweight.");
+		} else {
+			System.out.println("You are obese.");
+		}
+		
+		return Category;
 	}
 
 
